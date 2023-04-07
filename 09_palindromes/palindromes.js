1376ce08bf2d84674b603e592d031d300b402053
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2023-04-07 15:28:25 -0300

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, ""); ///reemplaza espacios en blancos por una string vacia 
+    const strReverse = string.split("").reverse().join("");
+    return string == strReverse;
+    
 };
 
 // Do not edit below this line
