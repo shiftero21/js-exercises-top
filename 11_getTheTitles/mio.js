f2c543f864f63222b7c8a166bbf1d61805a9e5c4
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2023-04-07 16:32:23 -0300

    Complete exercises

---
+++ 11_getTheTitles/mio.js
@@ -0,0 +1,16 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+
+  const getTheTitles = books.map(function (books){
+    return book.title;
+}) ;
+
+getTheTitles(books);
